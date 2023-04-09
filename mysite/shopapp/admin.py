e4a1e3d92d3e64b8b03020607553f6af204aedf3
Татьяна Чичканова <tanika135@mail.ru>
2023-04-09 22:18:39 +0300

    Добавлена группировка полей

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -1,14 +1,35 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Order
+
+
+class OrderInline(admin.TabularInline):
+    model = Product.orders.through
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderInline,
+    ]
     list_display = 'pk', 'name', 'description_short', 'price', 'discount'
     list_display_links = 'pk', 'name'
     ordering = 'pk', '-name',
     search_fields = 'name', 'description'
+    fieldsets = [
+        (None, {
+            'fields': ('name', 'description'),
+        }),
+        ('Price options', {
+            'fields': ('price', 'discount'),
+            'classes': ('collapse', 'wide'),
+        }),
+        ('Extra options', {
+            'fields': ('archived',),
+            'classes': ('collapse',),
+            'description': 'Extra options. Field "archived" is for soft delete.'
+        }),
+    ]
 
     def description_short(self, obj: Product) -> str:
         if len(obj.description) < 48:
@@ -17,3 +38,22 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 #admin.site.register(Product, ProductAdmin)
+
+# class ProductInline(admin.TabularInline):
+class ProductInline(admin.StackedInline):
+    model = Order.products.through
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductInline,
+    ]
+    list_display = 'delivery_address', 'promocode', 'created_at', 'user_verbose'
+
+    def get_queryset(self, request):
+        return Order.objects.select_related('user').prefetch_related('products')
+
+    def user_verbose(self, obj: Order) -> str:
+        return obj.user.first_name or obj.user.username
+
