49c0c02b62658f2923a6ce81db843c49324f3c58
Татьяна Чичканова <tanika135@mail.ru>
2023-04-09 21:20:06 +0300

    Добавлены фильтры и поле поиска

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -7,6 +7,8 @@ from .models import Product
 class ProductAdmin(admin.ModelAdmin):
     list_display = 'pk', 'name', 'description_short', 'price', 'discount'
     list_display_links = 'pk', 'name'
+    ordering = 'pk', '-name',
+    search_fields = 'name', 'description'
 
     def description_short(self, obj: Product) -> str:
         if len(obj.description) < 48:
