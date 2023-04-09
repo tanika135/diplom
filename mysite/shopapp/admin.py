e4ffd943fb3c48173b8a1e1047e083d7f4b04c55
Татьяна Чичканова <tanika135@mail.ru>
2023-04-09 21:14:33 +0300

    Подключены модели к админке

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = 'pk', 'name', 'description_short', 'price', 'discount'
+    list_display_links = 'pk', 'name'
+
+    def description_short(self, obj: Product) -> str:
+        if len(obj.description) < 48:
+            return obj.description
+        return obj.description[:48] + '...'
+
+
+#admin.site.register(Product, ProductAdmin)
