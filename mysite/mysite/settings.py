cbff4b4349aa9bf3198249ced73424047bba22a6
Татьяна Чичканова <tanika135@mail.ru>
2023-04-11 20:15:51 +0300

    Добавлена форма загрузки файлов и форма для ввода личных данных

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'shopapp.apps.ShopappConfig',
+    'requestdataapp.apps.RequestdataappConfig',
 ]
 
 MIDDLEWARE = [
---
+++ mysite/requestdataapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import process_get_view, user_form, handle_file_upload
+
+
+app_name = 'requestdataapp'
+
+urlpatterns = [
+    path('get/', process_get_view, name='get-view'),
+    path('bio/', user_form, name='user_form'),
+    path('upload/', handle_file_upload, name='handle_file_upload'),
+ ]
