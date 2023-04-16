8837b20c25288dcde1bb75467035bc5cc434f42b
Татьяна Чичканова <tanika135@mail.ru>
2023-04-16 23:07:15 +0300

    добавлена форма для создания заказов

--- mysite/shopapp/urls.py
+++ mysite/shopapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import shop_index, groups_list, products_list, orders_list, create_product
+from .views import shop_index, groups_list, products_list, orders_list, create_product, create_order
 
 app_name = 'shopapp'
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('products/', products_list, name='products_list'),
     path('orders/', orders_list, name='orders_list'),
     path('products/create', create_product, name='product_create'),
+    path('orders/create', create_order, name='create_order'),
 ]
--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import Group
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect, reverse
 
-from .forms import ProductForm
+from .forms import ProductForm, OrderForm
 from .models import Product, Order
 
 
@@ -39,7 +39,8 @@ def create_product(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
-            Product.objects.create(**form.cleaned_data)
+            # Product.objects.create(**form.cleaned_data)
+            form.save()
             url = reverse('shopapp:products_list')
             return redirect(url)
     else:
@@ -48,6 +49,21 @@ def create_product(request: HttpRequest) -> HttpResponse:
         'form': form,
     }
     return render(request,  'shopapp/create-product.html', context=context)
+
+
+def create_order(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            url = reverse('shopapp:orders_list')
+            return redirect(url)
+    else:
+        form = OrderForm()
+    context = {
+        'form': form,
+    }
+    return render(request,  'shopapp/create-order.html', context=context)
 
 
 def orders_list(request: HttpRequest):
