ba807eabac86ff89397b843552ddb64132f03530
Татьяна Чичканова <tanika135@mail.ru>
2023-04-20 20:52:22 +0300

    добавлены ListView и DetailView

--- mysite/shopapp/urls.py
+++ mysite/shopapp/urls.py
@@ -1,14 +1,25 @@
 from django.urls import path
 
-from .views import shop_index, groups_list, products_list, orders_list, create_product, create_order
+from .views import (
+    ShopIndexView,
+    GroupsListView,
+    ProductDetailsView,
+    ProductsListView,
+    OrderDetailView,
+    OrdersListView,
+    create_product,
+    create_order,
+)
 
 app_name = 'shopapp'
 
 urlpatterns = [
-    path('', shop_index, name='index'),
-    path('groups/', groups_list, name='groups_list'),
-    path('products/', products_list, name='products_list'),
-    path('orders/', orders_list, name='orders_list'),
+    path('', ShopIndexView.as_view(), name='index'),
+    path('groups/', GroupsListView.as_view(), name='groups_list'),
+    path('products/', ProductsListView.as_view(), name='products_list'),
+    path('products/<int:pk>/', ProductDetailsView.as_view(), name='product_details'),
+    path('orders/', OrdersListView.as_view(), name='orders_list'),
+    path('orders/<int:pk>/', OrderDetailView.as_view(), name='order_details'),
     path('products/create', create_product, name='product_create'),
     path('orders/create', create_order, name='create_order'),
 ]
--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -2,37 +2,73 @@ from timeit import default_timer
 
 from django.contrib.auth.models import Group
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView
 
 from .forms import ProductForm, OrderForm
+from .forms import GroupForm
 from .models import Product, Order
 
 
-def shop_index(request: HttpRequest):
-    products = [
-        ('Laptop', 1999),
-        ('Desktop', 2999)
-    ]
-    context = {
-        'time_running': default_timer(),
-        'products': products,
-    }
-    return render(request, 'shopapp/shop-index.html', context=context)
+class ShopIndexView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        products = [
+            ('Laptop', 1999),
+            ('Desktop', 2999),
+            ('Smartphone', 999),
+        ]
+        context = {
+            'time_running': default_timer(),
+            'products': products,
+        }
+        return render(request, 'shopapp/shop-index.html', context=context)
 
 
-def groups_list(request: HttpRequest):
-    context = {
-        'groups': Group.objects.prefetch_related('permissions').all(),
+class GroupsListView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        context = {
+            'form': GroupForm(),
+            'groups': Group.objects.prefetch_related('permissions').all(),
 
-    }
-    return render(request, 'shopapp/groups-list.html', context=context)
+        }
+        return render(request, 'shopapp/groups-list.html', context=context)
+
+    def post(self, request: HttpRequest):
+        form = GroupForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect(request.path)
 
 
-def products_list(request: HttpRequest):
-    context = {
-        'products': Product.objects.all(),
-    }
-    return render(request, 'shopapp/products-list.html', context=context)
+class ProductDetailsView(DetailView):
+    template_name = 'shopapp/product-details.html'
+    model = Product
+    context_object_name = 'product'
+
+    # def get(self, request: HttpRequest, pk: int) -> HttpResponse:
+    #     product = get_object_or_404(Product, pk=pk)
+    #     context = {
+    #         'product': product,
+    #     }
+    #     return render(request, 'shopapp/product-details.html', context=context)
+
+
+class ProductsListView(ListView):
+    template_name = 'shopapp/products-list.html'
+    model = Product
+    context_object_name = 'products'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['products'] = Product.objects.all()
+    #     return context
+
+# def products_list(request: HttpRequest):
+#     context = {
+#         'products': Product.objects.all(),
+#     }
+#     return render(request, 'shopapp/products-list.html', context=context)
 
 
 def create_product(request: HttpRequest) -> HttpResponse:
@@ -66,13 +102,28 @@ def create_order(request: HttpRequest) -> HttpResponse:
     return render(request,  'shopapp/create-order.html', context=context)
 
 
-def orders_list(request: HttpRequest):
-    context = {
-        'orders': Order.objects.select_related('user').prefetch_related('products').all(),
+# def orders_list(request: HttpRequest):
+#     context = {
+#         'orders': Order.objects.select_related('user').prefetch_related('products').all(),
+#
+#     }
+#     return render(request, 'shopapp/order_list.html', context=context)
 
-    }
-    return render(request, 'shopapp/orders-list.html', context=context)
 
+class OrdersListView(ListView):
+    queryset = (
+        Order.objects
+        .select_related('user')
+        .prefetch_related('products')
+    )
+
+
+class OrderDetailView(DetailView):
+    queryset = (
+        Order.objects
+        .select_related('user')
+        .prefetch_related('products')
+    )
 
 
     # print(request.path)
