c806214760eca6db9cdcce591b12aaf7a6fbd087
Татьяна Чичканова <tanika135@mail.ru>
2023-04-30 20:29:56 +0300

    интернационализацию во все таблицы в Django админ панели

--- 11_I18n/app_users/models.py
+++ 11_I18n/app_users/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
     pass
 
+    class Meta:
+        verbose_name_plural = _('users')
+        verbose_name = _('user')
