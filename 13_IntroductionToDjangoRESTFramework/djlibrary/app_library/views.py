d10200ccafc5958468591d6d0680662b836d09af
Татьяна <tanika135@mail.ru>
2023-05-02 22:48:28 +0300

    фильтрация книг и авторов

--- 13_IntroductionToDjangoRESTFramework/djlibrary/app_library/views.py
+++ 13_IntroductionToDjangoRESTFramework/djlibrary/app_library/views.py
@@ -10,8 +10,22 @@ class BookList(ListModelMixin, CreateModelMixin, GenericAPIView):
     def get_queryset(self):
         queryset = Book.objects.all()
         item_name = self.request.query_params.get('name')
-        if item_name:
-            queryset = queryset.filter(name=item_name)
+        author = self.request.query_params.get('author')
+        if item_name and author:
+            queryset = queryset.filter(name=item_name, author=author)
+
+        pages_equal = self.request.query_params.get('pages_eq')
+        if pages_equal:
+            queryset = queryset.filter(pages=pages_equal)
+
+        pages_larger_than = self.request.query_params.get('pages_gt')
+        if pages_larger_than:
+            queryset = queryset.filter(pages__gt=pages_larger_than)
+
+        pages_less_than = self.request.query_params.get('pages_lt')
+        if pages_less_than:
+            queryset = queryset.filter(pages__lt=pages_less_than)
+
         return queryset
 
     def get(self, request):
