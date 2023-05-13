38caae3cefbd85a4e4ccab8d190d708ef9e19575
Татьяна <tanika135@mail.ru>
2023-05-13 10:18:44 +0300

    тесты для cart

--- 12_DjangoCaching/djcaching/cart/tests.py
+++ 12_DjangoCaching/djcaching/cart/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from app_shops.models import Product, Shop, ShopStock
+
+
+class CartViewTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.product = Product.objects.create(name='Cart test product')
+        cls.shop = Shop.objects.create(name='Cart test shop')
+        cls.shopStock = ShopStock.objects.create(
+            shop=cls.shop,
+            product=cls.product,
+            stock=2,
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        cls.product.delete()
+        cls.shop.delete()
+        cls.shopStock.delete()
+
+    def test_cart_add(self):
+        print(reverse('cart:cart_add', kwargs={'product_id': self.product.pk}))
+        response = self.client.post(
+            reverse('cart:cart_add', kwargs={'product_id': self.product.pk}),
+            {
+                'shop': self.shop.pk
+            }
+        )
+        self.assertRedirects(response, reverse('cart:cart_detail'))
+
+    def test_cart_detail(self):
+        response = self.client.get(reverse('cart:cart_detail'))
+        self.assertTemplateUsed(response, 'cart/detail.html')
+
+    def test_cart_remove(self):
+        response = self.client.post(
+            reverse('cart:cart_remove', kwargs={'product_id': self.product.pk}),
+            {}
+        )
+        self.assertRedirects(response, reverse('cart:cart_detail'))
+
+
