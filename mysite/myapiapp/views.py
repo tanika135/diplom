bfa5d8fe5b73206ce29d767a5c4400c5d98ce390
Татьяна <tanika135@mail.ru>
2023-12-01 19:07:14 +0300

    добавлен Class Based Views

--- mysite/myapiapp/views.py
+++ mysite/myapiapp/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import Group
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework.views import APIView
 
 
 @api_view()
@@ -8,4 +10,8 @@ def hello_world_view(request: Request) -> Response:
     return Response({"message": "Hello, World!"})
 
 
-
+class GroupsListView(APIView):
+    def get(self, request: Request) -> Response:
+        groups = Group.objects.all()
+        data = [group.name for group in groups]
+        return Response({'groups': data})
