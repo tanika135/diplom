d7df7ca49694511c33180dd3a550891272f26d81
Татьяна <tanika135@mail.ru>
2023-12-05 18:02:41 +0300

    добавлен Mixins

--- mysite/myapiapp/views.py
+++ mysite/myapiapp/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth.models import Group
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView, ListCreateAPIView
+# from rest_framework.mixins import ListModelMixin
 
 from .serializers import GroupsSerializer
 
@@ -12,9 +13,20 @@ def hello_world_view(request: Request) -> Response:
     return Response({"message": "Hello, World!"})
 
 
-class GroupsListView(APIView):
-    def get(self, request: Request) -> Response:
-        groups = Group.objects.all()
-        serialized = GroupsSerializer(groups, many=True)
-        # data = [group.name for group in groups]
-        return Response({'groups': serialized.data})
+class GroupsListView(ListCreateAPIView):
+    """
+    Django REST Framework Mixins
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupsSerializer
+
+
+# class GroupsListView(APIView):
+#     """
+#     Django REST Framework Serializer
+#     """
+#     def get(self, request: Request) -> Response:
+#         groups = Group.objects.all()
+#         serialized = GroupsSerializer(groups, many=True)
+#         # data = [group.name for group in groups]
+#         return Response({'groups': serialized.data})
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -155,7 +155,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = reverse_lazy('myauth:profiles-list')
 LOGIN_URL = reverse_lazy('myauth:login')
 
-REST_FRAMEWORKS = {
+REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
