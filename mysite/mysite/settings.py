b8a1e2b1aab25b84998fe0ffb2589c4697b3eb29
Татьяна <tanika135@mail.ru>
2024-01-18 14:34:49 +0300

    добавлен quieries.sql

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -175,3 +175,25 @@ SPECTACULAR_SETTINGS = {
     'SERVE_INCLUDE_SCHEMA': False,
 }
 
+LOGGING = {
+    'version': 1,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+}
+
---
+++ mysite/quieries.sql
@@ -0,0 +1,19 @@
+SELECT "shopapp_product"."id",
+       "shopapp_product"."name",
+       "shopapp_product"."description",
+       "shopapp_product"."price",
+       "shopapp_product"."discount",
+       "shopapp_product"."created_at",
+       "shopapp_product"."archived",
+       "shopapp_product"."created_by_id",
+       "shopapp_product"."preview"
+FROM "shopapp_product"
+WHERE NOT "shopapp_product"."archived"
+ORDER BY "shopapp_product"."name" ASC, "shopapp_product"."price" ASC;
+
+SELECT "shopapp_productimage"."id",
+       "shopapp_productimage"."product_id",
+       "shopapp_productimage"."image",
+       "shopapp_productimage"."description"
+FROM "shopapp_productimage"
+WHERE "shopapp_productimage"."product_id" IN (2);
