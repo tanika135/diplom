b4bbcfd3a1ca946850750f4d8e1f114211dc9b23
Татьяна <tanika135@mail.ru>
2024-02-12 18:45:11 +0300

    добавлен transaction.atomic

--- mysite/shopapp/management/commands/create_order.py
+++ mysite/shopapp/management/commands/create_order.py
@@ -1,16 +1,25 @@
+from typing import Sequence
+
 from django.contrib.auth.models import User
 from django.core.management import BaseCommand
+from django.db import transaction
 
-from shopapp.models import Order
+from shopapp.models import Order, Product
 
 
 class Command(BaseCommand):
+    @transaction.atomic
     def handle(self, *args, **options):
-        self.stdout.write('Create order')
+        self.stdout.write('Create order with products')
         user = User.objects.get(username='admin')
-        order = Order.objects.get_or_create(
-            delivery_address='st Sesame',
-            promocode='SALE123',
+        products: Sequence[Product] = Product.objects.all()
+        order, created = Order.objects.get_or_create(
+            delivery_address='st University, 1',
+            promocode='SALE1',
             user=user,
         )
+
+        for product in products:
+            order.products.add(product)
+        order.save()
         self.stdout.write(f'Created order {order}')
