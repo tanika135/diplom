3271ebb568c82b3abb8b8aed25f8f7be7be28c48
Татьяна <tanika135@mail.ru>
2024-02-16 21:26:36 +0300

    добавлена функция для обновления сразу нескольких товаров

--- mysite/shopapp/management/commands/bulk_actions.py
+++ mysite/shopapp/management/commands/bulk_actions.py
@@ -8,20 +8,28 @@ from shopapp.models import Product
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write('Start demo bulk actions')
+        """
+        Обновляет скидку у всех товаров с названием 'Smartphone'
+        """
+        result = Product.objects.filter(
+            name__contains='Smartphone',
+        ).update(discount=10)
 
-        info = [
-            ('Smartphone 1', 199),
-            ('Smartphone 2', 299),
-            ('Smartphone 3', 399),
-        ]
-        products = [
-            Product(name=name, price=price)
-            for name, price in info
-        ]
+        print(result)
 
-        result = Product.objects.bulk_create(products)
-
-        for obj in result:
-            print(obj)
+        # info = [
+        #     ('Smartphone 1', 199),
+        #     ('Smartphone 2', 299),
+        #     ('Smartphone 3', 399),
+        # ]
+        # products = [
+        #     Product(name=name, price=price)
+        #     for name, price in info
+        # ]
+        #
+        # result = Product.objects.bulk_create(products)
+        #
+        # for obj in result:
+        #     print(obj)
 
         self.stdout.write(f'Done')
