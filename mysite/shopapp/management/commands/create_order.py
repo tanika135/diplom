31df803370130d94abe3b2dcdd3381c819df398e
Татьяна <tanika135@mail.ru>
2024-02-16 21:17:14 +0300

    добавлен bulk_actions

--- mysite/shopapp/management/commands/create_order.py
+++ mysite/shopapp/management/commands/create_order.py
@@ -12,10 +12,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write('Create order with products')
         user = User.objects.get(username='admin')
-        products: Sequence[Product] = Product.objects.all()
+        # products: Sequence[Product] = Product.objects.defer('description', 'price', 'created_at').all()
+        products: Sequence[Product] = Product.objects.only('id').all()
         order, created = Order.objects.get_or_create(
             delivery_address='st University, 1',
-            promocode='SALE1',
+            promocode='SALE90',
             user=user,
         )
 
